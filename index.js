eb087b605b969cb0251dbedafe679ecf2aace337
Cesar Colunga <cesara.colunga@gmail.com>
2022-12-18 14:42:40 -0700

    commit tree 7e2c2d6fd0086ec26a2cb167f1701b17326cea19

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function introduction(name) {
-    return `Hi, my name is ${name}`;
+    const string = `Hi, my name is ${name}.`;
+    //console.log(`Hi, my name is ${name}`);
+    return string;
 }
 
 function introductionWithLanguage (name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}`;
+    //console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+    const string = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return string;
 }
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    `Hi, my name is ${name} and I am learning to program in ${language}`;
+    //console.log(`Hi, my name is ${name} and I am learning to program in ${language}`);
+    const string = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return string;
 }
+
+
+/* Used this to test code
+console.log(introduction("Cesar"));
+console.log(introductionWithLanguage("Cesar","C++"));
+console.log(introductionWithLanguageOptional("Cesar"));
+*/
